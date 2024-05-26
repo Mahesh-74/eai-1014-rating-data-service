326979fb4498f296c41d5c7143546e8aa34cccfb
Mahesh-74 <mahesh74.java@gmail.com>
2024-05-26 20:16:33 +0530

    FM

---
+++ src/main/java/com/rating/data/service/controller/RatingResource.java
@@ -0,0 +1,34 @@
+package com.rating.data.service.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rating.data.service.model.Rating;
+import com.rating.data.service.model.UserRating;
+
+@RestController
+@RequestMapping("/ratingsData")
+public class RatingResource {
+
+	@GetMapping("/{movieId}")
+	public Rating getRatings(@PathVariable("movieId") String movieId) {
+		return new Rating(movieId, 4);
+
+	}
+	
+	@GetMapping("/user/{userId}")
+	public UserRating getUserRatings(@PathVariable("userId") String userId) {
+		List<Rating> ratings = Arrays.asList(new Rating("1234", 4), new Rating("5678", 3));
+		UserRating userRating = new UserRating();
+		userRating.setUserRating(ratings);
+		return userRating;
+
+	}
+	
+
+}
---
+++ src/test/java/com/rating/data/service/eai_1014_rating_data_service/Eai1014RatingDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rating.data.service.eai_1014_rating_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eai1014RatingDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
